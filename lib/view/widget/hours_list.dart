19d78bdcabf4bf041399fe3d032853763a53d1c5
amin.khademi <amin.khademi2003@gmail.com>
2023-12-12 05:30:20 +0330

    create hourlist class

---
+++ lib/view/widget/hours_list.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:weather_app/viewModel/controller/home.controller.dart';
+
+class HoursList extends StatelessWidget {
+  HoursList({super.key});
+  final controller = Get.put(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 180,
+      child: ListView.builder(
+        itemCount: controller.model.value!.days![0].hours!.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () => controller.setHour(index),
+            child: Column(
+              children: [
+                Obx(
+                  () => Card(
+                    elevation: 10,
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    shadowColor: controller.compareIndex(index)
+                        ? Colors.blue
+                        : Colors.white70,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Obx(() => Container(
+                          height: 130,
+                          width: 80,
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(
+                            color: controller.compareIndex(index)
+                                ? Colors.blue
+                                : Colors.white70,
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Obx(
+                                () => Text(
+                                  controller.getHour(index),
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: controller.compareIndex(index)
+                                          ? Colors.white
+                                          : Colors.grey),
+                                ),
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
